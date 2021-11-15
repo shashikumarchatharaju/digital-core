b6376a9702e28286e0abe0f2f9d25990eb7ceb34
ShashiKanth <57086246+shashikanth-t@users.noreply.github.com>
2021-11-15 20:48:18 +0530

    first commit
    first commit

---
+++ src/main/java/digitalApp.java
@@ -0,0 +1,9 @@
+
+public class digitalApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Welcome To Digital Core Application Demo...");
+	}
+
+}
